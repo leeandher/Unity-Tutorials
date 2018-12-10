fcd5e2208993a07697fd73b7a5c2cc268001f27a
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-12-09 22:20:57 -0500

    Set up asteroids

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary") { return; }
+        // Note: Objects are marked to be destroyed, not actually destroyed
+        // therefore order does not matter
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, transform.position, transform.rotation);
+        }
+    }
+}
